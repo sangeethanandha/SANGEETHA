2bb6c4536e734d81035a1c8fb118283edfe1ae53
sangeethanandha <49307182+sangeethanandha@users.noreply.github.com>
2019-04-10 22:34:39 +0530

    b4.py

---
+++ b4.py
@@ -0,0 +1,5 @@
+n=input("enter your input")
+if(n=='a' or n=='b' or n=='c' or n=='d' or n=='e' or n=='f' or n=='g' or n=='h' or n=='i' or n=='j' or n=='k'or n=='l' or n=='m' or n=='n' or n=='o' or n=='p' or n=='q' or n=='r' or n=='s' or n=='t' or n=='u' or n=='v' or n=='w' or n=='x' or n=='y' or n=='z'): 
+  print("alphabet")
+else:
+  print("invalid")
