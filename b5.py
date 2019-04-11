e1c57159de41a28dfe352063a7c1f5029c0133a6
sangeethanandha <49307182+sangeethanandha@users.noreply.github.com>
2019-04-11 12:10:50 +0530

    b5.py

---
+++ b5.py
@@ -0,0 +1,9 @@
+a=int(input("enter the first number"))
+b=int(input("enter the second number"))
+c=int(input("enter the third number"))
+if((a>b) and (a>c)):
+  print("a is greatest number")
+elif((b>a) and (b>c)):
+  print("b is greatest number")
+else:
+  print("c is greatest")
