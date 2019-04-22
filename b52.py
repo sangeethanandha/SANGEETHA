2733806bc2822c789e1143c72a4a28425ee884fd
sangeethanandha <49307182+sangeethanandha@users.noreply.github.com>
2019-04-22 21:25:06 +0530

    b52.py

---
+++ b52.py
@@ -0,0 +1,24 @@
+n=int(input("enter the number"))
+if (n>=1 and n<=10):
+  if n==1:
+    print("one")
+  elif n==2:
+    print("two")
+  elif n==3:
+    print("three")
+  elif n==4:
+    print("four")
+  elif n==5:
+    print("five")
+  elif n==6:
+    print("six")
+  elif n==7:
+    print("seven")
+  elif n==8:
+    print("eight")
+  elif n==9:
+    print("nine")
+  elif n==10:
+    print("ten")
+  else:
+    print("invalid")
