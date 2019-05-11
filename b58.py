09583e25e53b9560f8497bdbd4a1c1a0621d9e20
sangeethanandha <49307182+sangeethanandha@users.noreply.github.com>
2019-05-11 13:55:54 +0530

    b58.py

---
+++ b58.py
@@ -0,0 +1,13 @@
+n,k=[int(x) for x in input().split()]
+lst=[]
+count=0
+for i in range(0,n):
+  m=int(input())
+  lst.append(m)
+for j in lst:
+  if j==k:
+    count=count+1
+if count>0:
+  print("yes")
+else:
+  print("no")
