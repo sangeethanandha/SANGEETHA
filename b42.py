a21ffeb219405378f4a78c9381d290ea9dccedb6
sangeethanandha <49307182+sangeethanandha@users.noreply.github.com>
2019-05-11 12:22:46 +0530

    b42.py

---
+++ b42.py
@@ -0,0 +1,11 @@
+s1=input("enter the string1")
+s2=input("enter the string2")
+ss1=len(s1)
+ss2=len(s2)
+if ss1<=1000 and ss2<=1000:
+  if ss1==ss2:
+    print(s1)
+  if ss1>ss2:
+    print(s1)
+  else:
+    print(s2)
